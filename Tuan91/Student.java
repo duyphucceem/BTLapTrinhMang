086ac9c113ee9561aa2abf2b0403ad2b4cba50c3
duyphucceem <93960027+duyphucceem@users.noreply.github.com>
2024-03-21 23:47:59 +0700

    Create Student.java

---
+++ Tuan91/Student.java
@@ -0,0 +1,33 @@
+
+package Tuan91;
+
+import java.io.Serializable;
+
+
+public class Student implements Serializable{
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
